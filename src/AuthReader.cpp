3b17fa0e074d7462cc763c1d2aa83ae0db96f807
7dix <tenoramarek@gmail.com>
2024-09-28 10:55:36 +0000

    feat: auth file is loading correctly

--- src/AuthReader.cpp
+++ src/AuthReader.cpp
@@ -1,5 +1,6 @@
 // AuthReader.cpp
 #include "AuthReader.h"
+#include <iostream>
 #include <fstream>
 #include <sstream>
 #include <stdexcept>
@@ -7,21 +8,35 @@
 AuthReader::AuthReader(const std::string authFile) : authFile_(authFile) {}
 
 AuthData AuthReader::read() {
+    // Open the authentication file
+
     std::ifstream file(authFile_);
     if (!file.is_open()) {
-        throw std::runtime_error("Soubor s autentizačními údaji se nepodařilo otevřít");
+        throw std::runtime_error("Failed to open the authentication file");
     }
 
     AuthData authData;
     std::string line;
     while (std::getline(file, line)) {
+        std::cout << "Line: " << line << std::endl;
         std::istringstream iss(line);
-        std::string username;
-        std::string password;
-        if (!(iss >> username >> password)) {
-            throw std::runtime_error("Chyba při čtení autentizačního souboru");
+        std::string key, value;
+        if (std::getline(iss, key, '=') && std::getline(iss, value)) {
+
+            // Remove leading and trailing spaces from key and value
+            key.erase(0, key.find_first_not_of(' '));
+            key.erase(key.find_last_not_of(' ') + 1);
+            value.erase(0, value.find_first_not_of(' '));
+            value.erase(value.find_last_not_of(' ') + 1);
+
+            if (key == "username") {
+                authData.username = value;
+            } else if (key == "password") {
+                authData.password = value;
+            }
+        } else {
+            throw std::runtime_error("Error reading the authentication file");
         }
-        authData = {username, password};
     }
 
     return authData;
--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,9 @@ int main(int argc, char* argv[]) {
     ArgumentsParser argumentsParser;
     ProgramOptions options = argumentsParser.parse(argc, argv);
 
-    exit(EXIT_SUCCESS);
     AuthReader authReader(options.authFile);
     AuthData authData = authReader.read();
-    std::cout << "Loaded: " << authData.username << " " << authData.password << std::endl;
+    std::cout << "Name: '" << authData.username << "' password: '" << authData.password << "'" << std::endl;
 
     if (options.useTLS) {
         std::cout << "Loading SSL certificates.." << std::endl;
